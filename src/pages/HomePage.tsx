da595ecc3bbbca17715ca7c82b3b43734d6fa76a
Minhas <amarmin@amazon.de>
2026-07-22 13:38:55 +0200

    fix: Google Maps URL korrigiert fuer Hornissenweg Stammheim

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -160,13 +160,12 @@ export function HomePage(): JSX.Element {
           {/* Google Maps Karte */}
           <div className="mt-8 overflow-hidden rounded-lg border border-border">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.5!2d9.195!3d48.8425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799db5b3b0a9c9d%3A0x3a1234567890!2sStammheim%2C+Stuttgart!5e0!3m2!1sde!2sde!4v1"
+              src="https://maps.google.com/maps?q=Hornissenweg,+70439+Stuttgart+Stammheim&t=&z=16&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="350"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
               title="Standort der WG in Stuttgart-Stammheim auf Google Maps"
               className="w-full"
             />
